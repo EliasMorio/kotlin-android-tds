b65a56b3f9c32e9a09247e1a1f40520aa7ac69a9
EliasMorio <elias14160@gmail.com>
2023-01-24 20:15:00 +0100

    Finishing AcceuilView.kt

--- app/src/main/java/fr/eliasmorio/td1/AcceuilView.kt
+++ app/src/main/java/fr/eliasmorio/td1/AcceuilView.kt
@@ -1,12 +1,42 @@
 package fr.eliasmorio.td1
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
 
 class AcceuilView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_acceuil_view)
+
+        val btns = arrayOf(findViewById<ImageButton>(R.id.btnWalk),
+            findViewById<ImageButton>(R.id.btnBike),
+            findViewById<ImageButton>(R.id.btnCar),
+            findViewById<ImageButton>(R.id.btnBus),
+            findViewById<ImageButton>(R.id.btnTrain),
+            findViewById<ImageButton>(R.id.btnPlane));
+
+        fun onClick(v: View) {
+            for (btn in btns) {
+                if (btn == v) {
+                    btn.setColorFilter(Color.BLUE)
+                } else {
+                    btn.setColorFilter(Color.LTGRAY)
+                }
+            }
+            findViewById<TextView>(R.id.transportTitle).text =  "Transport choisi : " + v.contentDescription
+        }
+
+        for (btn in btns) {
+            btn.setOnClickListener {
+                onClick(it)
+            }
+        }
+
 
 
     }
