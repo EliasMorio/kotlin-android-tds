a710ef5ef234e5146f92e411e4d67beff45ad293
EliasMorio <elias14160@gmail.com>
2023-02-09 09:57:35 +0100

    TP4

--- app/src/main/java/but/info/tp4/MainActivity.kt
+++ app/src/main/java/but/info/tp4/MainActivity.kt
@@ -1,16 +1,22 @@
 package but.info.tp4
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import but.info.tp4.ui.*
 import but.info.tp4.ui.theme.TP4Theme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +28,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    val (recette, setRecette) = remember { mutableStateOf<IRecette>(GratinPates()) }
+                    val drawerState = rememberDrawerState( initialValue = DrawerValue.Closed )
+                    val scope = rememberCoroutineScope()
+                    ModalDrawer(drawerContent = {Menu(recette, setRecette) { scope.launch { drawerState.close() } } }, drawerState = drawerState, ) {
+                        val (page, setPage) = remember { mutableStateOf(Page.RESUME) }
+                        Recette(page, setPage, recette) { scope.launch { drawerState.open() } }
+                    }
                 }
             }
         }
@@ -30,14 +42,58 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Menu(selectedRecette: IRecette, setRecette: IRecette.() -> Unit, close: () -> Unit){
+    val recettes = HashMap<String, IRecette>()
+    recettes["Gratin de pâtes"] = GratinPates()
+    recettes["Caponata"] = Caponata()
+    recettes["Madeleines"] = Madeleines()
+    recettes["Stollen"] = Stollen()
+    val selectedModifier = Modifier.background(MaterialTheme.colors.primary)
+
+    Column() {
+        IconButton(onClick = { close() }) {
+            Icon(painter = painterResource(id = R.drawable.baseline_close_24), contentDescription = "Fermer")
+        }
+        for (recette in recettes) {
+            TextButton(onClick = {
+                setRecette(recette.value)
+                close()},
+                modifier = if (recette.value == selectedRecette) selectedModifier else Modifier) {
+                Text(text = recette.key)
+            }
+
+        }
+    }
 }
 
-@Preview(showBackground = true)
+
+
 @Composable
-fun DefaultPreview() {
-    TP4Theme {
-        Greeting("Android")
+fun Recette(page: Page, setPage: (Page) -> Unit, recette: IRecette, clickMenu: () -> Unit ){
+    Column() {
+        val selectedColor = ButtonDefaults.outlinedButtonColors(backgroundColor = MaterialTheme.colors.secondary)
+
+        Row(horizontalArrangement = Arrangement.SpaceEvenly) {
+            IconButton(onClick = { clickMenu() },  ) {
+                Icon(painter = painterResource(id = R.drawable.ic_menu), contentDescription = "Menu")
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            OutlinedButton(onClick = { setPage(Page.RESUME) }, colors = if (page == Page.RESUME) selectedColor else ButtonDefaults.outlinedButtonColors()) {
+                Text(text = "Résumé")
+            }
+            OutlinedButton(onClick = { setPage(Page.INGREDIENTS) }, colors = if (page == Page.INGREDIENTS) selectedColor else ButtonDefaults.outlinedButtonColors()) {
+                Text(text ="Ingrédients")
+            }
+            OutlinedButton(onClick = { setPage(Page.RECETTE) }, colors = if (page == Page.RECETTE) selectedColor else ButtonDefaults.outlinedButtonColors()) {
+                Text(text = "Recette")
+            }
+            Spacer(modifier = Modifier.weight(1f))
+
+        }
+        when (page) {
+            Page.RESUME -> recette.Resume(modifier = Modifier.fillMaxSize())
+            Page.INGREDIENTS -> recette.Ingredients(modifier = Modifier.fillMaxSize())
+            Page.RECETTE -> recette.Recette(modifier = Modifier.fillMaxSize())
+        }
     }
 }
---
+++ app/src/main/java/but/info/tp4/Page.kt
@@ -0,0 +1,10 @@
+package but.info.tp4
+
+import but.info.tp4.ui.IRecette
+
+enum class Page {
+    RESUME,
+    INGREDIENTS,
+    RECETTE
+
+}
